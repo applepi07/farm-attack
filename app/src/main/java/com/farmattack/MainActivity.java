e6ad8c0ec6812d23aeac1f6b0106856fa6fe2eec
Charlie Pai <mail4charliepai@gmail.com>
2014-09-06 16:37:17 -0700

    Game loop

--- app/src/main/java/com/farmattack/MainActivity.java
+++ app/src/main/java/com/farmattack/MainActivity.java
@@ -2,35 +2,38 @@ package com.farmattack;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        // requesting to turn the title OFF
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // making it full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // set our MainGamePanel as the View
+        setContentView(new MainGamePanel(this));
+        Log.d(TAG, "View added");
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    protected void onDestroy() {
+        Log.d(TAG, "Destroying...");
+        super.onDestroy();
     }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "Stopping...");
+        super.onStop();
+    }
+
+
 }
